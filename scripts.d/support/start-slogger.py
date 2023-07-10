991e9e9d9488a7a6e9995cfe1de013f3cb9e4b07
Pascal Obry <pascal.obry@edf.fr>
2023-07-10 09:35:39 +0200

    Add support for user & session ids.

--- scripts.d/support/start-slogger.py
+++ scripts.d/support/start-slogger.py
@@ -1,5 +1,6 @@
 import subprocess
 import os.path
+from datetime import datetime
 
 # get the pid for the current process
 PID = os.getpid()
@@ -7,8 +8,9 @@ UID = os.getuid()
 
 WEB_SERVER = "__WEB_SERVER__"
 SALOME_VERSION = "__SALOME_VERSION__"
+USER_ID = "__USERID__"
 LOG_FILENAME = "/tmp/%s-salome.log" % UID
-
+SESSION_ID = datetime.now().strftime("%Y%m%d%H%M%S")
 # set GUI_LOG_FILE which is used in KERNEL to enable logging
 os.environ["GUI_LOG_FILE"] = LOG_FILENAME
 
@@ -20,11 +22,13 @@ def init(context, root_dir):
     SALOME_VERSION = os.environ["SALOME_VERSION"]
     SALOME_LOGGER = os.environ["LOGGER_ROOT_DIR"]
 
-    L_BIN=os.path.join(SALOME_LOGGER, "bin", "SalomeLogger")
-    L_PLG=os.path.join(SALOME_LOGGER, "bin", "libFilterPlugin.so")
+    L_BIN = os.path.join(SALOME_LOGGER, "bin", "SalomeLogger")
+    L_PLG = os.path.join(SALOME_LOGGER, "bin", "libFilterPlugin.so")
+
+    PFX = USER_ID + ',' + SESSION_ID + ',' + SALOME_VERSION
 
     subprocess.Popen([L_BIN,
-                      "--prefix", SALOME_VERSION,
+                      "--prefix", PFX,
                       "--server", WEB_SERVER,
                       "--lib-path", L_PLG,
                       "--pid", str(PID),
