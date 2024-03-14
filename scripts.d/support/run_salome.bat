e83a468c7c37dc254699b009d892f83b3d952d08
Pascal Obry <pascal.obry@edf.fr>
2024-03-14 13:05:09 +0100

    Fix windows support for building SALOME.
    It is now possible to build SALOME on Windows with logger
    and paravisaddons EDF.

---
+++ scripts.d/support/run_salome.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+
+REM Synopsis
+REM Name: run_salome.bat
+REM Options: SALOME arguments if any
+REM Description: runs the SALOME launcher
+REM Alternative: user can run the following commands a cmd terminal
+REM              cd <SALOME installation directory>
+REM              <SALOME installation directory>\W64\Python\python3.exe salome 
+REM
+
+SET out_dir_Path=%~dp0
+
+SET PYTHONIOENCODING=UTF_8
+SET PYTHONPATH=
+SET PYTHONPATH=%out_dir_Path%\W64\Python\lib;%PYTHONPATH%
+SET PYTHONPATH=%out_dir_Path%\W64\Python\lib\site-packages;%PYTHONPATH%
+SET PYTHONPATH=%out_dir_Path%\W64\sip\Lib\site-packages;%PYTHONPATH%
+SET PYTHONPATH=%out_dir_Path%\W64\PyQt;%PYTHONPATH%
+SET PYTHONPATH=%out_dir_Path%\W64\PyQt\lib\site-packages;%PYTHONPATH%
+
+SET PATH=%out_dir_Path%\W64\Python;%PATH%
+SET PATH=%out_dir_Path%\W64\Python\libs;%PATH%
+SET PATH=%out_dir_Path%\W64\Python\Scripts;%PATH%
+SET PATH=%out_dir_Path%\W64\pthreads\lib;%PATH%
+SET PATH=%out_dir_Path%\W64\EXT\bin;%PATH%
+SET PATH=%out_dir_Path%\W64\EXT\lib;%PATH%
+SET PATH=%out_dir_Path%\W64\qt\bin;%PATH%
+SET PATH=%out_dir_Path%\W64\sip\scripts;%PATH%
+SET PATH=%out_dir_Path%\W64\PyQt\bin;%PATH%
+
+REM Check if opengl32.dll is present, if yes remove it.
+SET SALOME_GUI_ROOT_DIR=%out_dir_Path%\W64\GUI\bin\salome
+IF EXIST "%SALOME_GUI_ROOT_DIR%\opengl32.dll" (
+ DEL /Q /S "%SALOME_GUI_ROOT_DIR%\opengl32.dll"
+)
+START %out_dir_Path%\W64\Python\python3.exe %out_dir_Path%\salome %*
